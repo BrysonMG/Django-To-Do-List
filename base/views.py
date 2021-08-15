90f84681fc0e086392b9b1cfe1c8840fba9b18ad
Bryson Goins <brysonmg@gmail.com>
2021-08-15 13:24:50 -0500

    view existing list entries

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Task
 
 
-def taskList(request):
-    return HttpResponse('To Do List')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
